b299d8761d67dbef95d3c4d5c944940a8d919e98
Farhad Modaresi <sfmth0@gmail.com>
2023-04-07 17:59:36 +0330

    Update pic.rb

--- pic.rb
+++ pic.rb
@@ -1,1 +1,1 @@
-RBA::Application.instance.main_window.current_view.save_image("./multi_macro.png",20000,20000)
+RBA::Application.instance.main_window.current_view.save_image("./multi_macro.png",2000,2000)
